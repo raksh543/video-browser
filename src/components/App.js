b4dad015d2305f0f98d32407a0423166f72bfe29
raksh543 <rakshitajain777@gmail.com>
2020-08-04 20:50:59 +0530

    adding styles to videolist and items

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import SearchBar from './SearchBar';
-import youtube from '../apis/youtube'
+import youtube from '../apis/youtube';
+import VideoList from './VideoList';
 
 const KEY ='AIzaSyBh4aXInbCnNlGW01Xz4zlo4xPMjdu1Z2g';
 
 class App extends React.Component{
-    onTermSubmit = (term) => {
-        youtube.get('/search',{
+    state = {videos : []};
+
+    onTermSubmit = async term => {
+        const response = await youtube.get('/search',{
         params: {
             part :'snippet',
             type: 'video',
@@ -14,12 +17,16 @@ class App extends React.Component{
             key: KEY,
             q: term
         }});
+        console.log(response);
+        this.setState({videos : response.data.items})
     };
 
     render(){
         return(
             <div className="ui container">
                 <SearchBar onTermSubmit={this.onTermSubmit} />
+                I have {this.state.videos.length} Videos.
+                <VideoList videos={this.state.videos}/>
             </div>
         );
     }
