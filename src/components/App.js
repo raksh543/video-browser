b764c6e30026dc51dba5157eca15b9d879c566f4
raksh543 <rakshitajain777@gmail.com>
2020-08-04 23:35:04 +0530

    fixing view

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,10 @@ const KEY ='AIzaSyBh4aXInbCnNlGW01Xz4zlo4xPMjdu1Z2g';
 class App extends React.Component{
     state = {videos : [], selectedVideo: null};
 
+    componentDidMount(){
+        this.onTermSubmit('buildings');
+    }
+
     onTermSubmit = async term => {
         const response = await youtube.get('/search',{
         params: {
@@ -19,7 +23,7 @@ class App extends React.Component{
             q: term
         }});
         console.log(response);
-        this.setState({videos : response.data.items})
+        this.setState({videos : response.data.items, selectedVideo: response.data.items[0]})
     };
 
     onVideoSelect = (video) => {
@@ -32,8 +36,16 @@ class App extends React.Component{
             <div className="ui container">
                 <SearchBar onTermSubmit={this.onTermSubmit} />
                 {/* I have {this.state.videos.length} Videos. */}
-                <VideoDetail video={this.state.selectedVideo}/>
-                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo}/>
+                        </div>
+                        <div className="five wide column">
+                            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
