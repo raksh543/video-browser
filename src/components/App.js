a9bee90ef7241a23f07a82b6062a0d16708a6476
raksh543 <rakshitajain777@gmail.com>
2020-08-04 21:48:03 +0530

    Adding video detail

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 const KEY ='AIzaSyBh4aXInbCnNlGW01Xz4zlo4xPMjdu1Z2g';
 
 class App extends React.Component{
-    state = {videos : []};
+    state = {videos : [], selectedVideo: null};
 
     onTermSubmit = async term => {
         const response = await youtube.get('/search',{
@@ -21,12 +22,18 @@ class App extends React.Component{
         this.setState({videos : response.data.items})
     };
 
+    onVideoSelect = (video) => {
+        // console.log('From the app', video)
+        this.setState({selectedVideo: video});
+    }
+
     render(){
         return(
             <div className="ui container">
                 <SearchBar onTermSubmit={this.onTermSubmit} />
-                I have {this.state.videos.length} Videos.
-                <VideoList videos={this.state.videos}/>
+                {/* I have {this.state.videos.length} Videos. */}
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
             </div>
         );
     }
