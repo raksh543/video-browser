48679fa585ecbe60c61cc0022106e809ec0a5b05
Raj kothari <rkmeansrk39@gmail.com>
2020-10-01 05:08:34 +0530

    code formatting

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,61 @@
-import React from 'react';
-import SearchBar from './SearchBar';
-import youtube from '../apis/youtube';
-import VideoList from './VideoList';
-import VideoDetail from './VideoDetail';
+import React from "react";
+import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
-const KEY ='AIzaSyBh4aXInbCnNlGW01Xz4zlo4xPMjdu1Z2g';
+const KEY = "AIzaSyBh4aXInbCnNlGW01Xz4zlo4xPMjdu1Z2g";
 
-class App extends React.Component{
-    state = {videos : [], selectedVideo: null};
+class App extends React.Component {
+  state = { videos: [], selectedVideo: null };
 
-    componentDidMount(){
-        this.onTermSubmit('buildings');
-    }
+  componentDidMount() {
+    this.onTermSubmit("buildings");
+  }
 
-    onTermSubmit = async term => {
-        const response = await youtube.get('/search',{
-        params: {
-            part :'snippet',
-            type: 'video',
-            maxResults: 5,
-            key: KEY,
-            q: term
-        }});
-        console.log(response);
-        this.setState({videos : response.data.items, selectedVideo: response.data.items[0]})
-    };
+  onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", {
+      params: {
+        part: "snippet",
+        type: "video",
+        maxResults: 5,
+        key: KEY,
+        q: term,
+      },
+    });
+    console.log(response);
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
+  };
 
-    onVideoSelect = (video) => {
-        // console.log('From the app', video)
-        this.setState({selectedVideo: video});
-    }
+  onVideoSelect = (video) => {
+    // console.log('From the app', video)
+    this.setState({ selectedVideo: video });
+  };
 
-    render(){
-        return(
-            <div className="ui container">
-                <SearchBar onTermSubmit={this.onTermSubmit} />
-                {/* I have {this.state.videos.length} Videos. */}
-                <div className="ui grid">
-                    <div className="ui row">
-                        <div className="eleven wide column">
-                            <VideoDetail video={this.state.selectedVideo}/>
-                        </div>
-                        <div className="five wide column">
-                            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
-                        </div>
-                    </div>
-                </div>
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onTermSubmit={this.onTermSubmit} />
+        {/* I have {this.state.videos.length} Videos. */}
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
             </div>
-        );
-    }
-};
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
